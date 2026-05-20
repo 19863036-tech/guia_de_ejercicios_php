368b7a820f0e841a08de9b6047c5cbb2cc06c85e
19863036-tech <19863036@clases.edu.sv>
2026-05-20 08:31:37 -0600

    ejercicio 11
    Cree un formulario que solicite tres notas de un estudiante y envíe los datos a otro
    archivo PHP donde se calcule el promedio y se indique si el estudiante aprobó o
    reprobó.

---
+++ destino11 - copia (2).php
@@ -0,0 +1,12 @@
+<?php
+$nota1 = $_POST ['nota1'];
+$nota2 = $_POST ['nota2'];
+$nota3 = $_POST ['nota3'];
+$promedio = ($nota1 + $nota2 + $nota3) / 3 ;
+if ($promedio >=7.0){
+    echo "has aprobado:";
+} else {
+     echo "reprobado:";
+}
+echo "su promedio es :" . $promedio
+?>
